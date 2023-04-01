e483a58592bad12b2da64c62f6b75b2532a03036
eniolajayi <ajayieniola16@gmail.com>
2023-04-01 16:13:54 +0100

    move array of button keys out of component

--- Keyboard.jsx
+++ Keyboard.jsx
@@ -1,110 +1,7 @@
-function Keyboard({ onButtonPressed }) {
+function Keyboard({ onButtonPressed, buttonKeys = KEYS }) {
   const handleButtonClick = (event, data) => {
     onButtonPressed(data);
   };
-
-  const buttonKeys = [
-    {
-      type: "operator",
-      symbol: "CE",
-      name: "clear-entry",
-    },
-    {
-      type: "operator",
-      symbol: "C",
-      name: "clear-all",
-    },
-    {
-      type: "operator",
-      symbol: "%",
-      name: "percent",
-    },
-    {
-      type: "operator",
-      symbol: "÷",
-      name: "divide",
-    },
-    {
-      type: "number",
-      symbol: "7",
-      name: "seven",
-    },
-    {
-      type: "number",
-      symbol: "8",
-      name: "eight",
-    },
-    {
-      type: "number",
-      symbol: "9",
-      name: "nine",
-    },
-    {
-      type: "operator",
-      symbol: "×",
-      name: "multiply",
-    },
-    {
-      type: "number",
-      symbol: "4",
-      name: "four",
-    },
-    {
-      type: "number",
-      symbol: "5",
-      name: "five",
-    },
-    {
-      type: "number",
-      symbol: "6",
-      name: "six",
-    },
-    {
-      type: "operator",
-      symbol: "-",
-      name: "subtract",
-    },
-    {
-      type: "number",
-      symbol: "1",
-      name: "one",
-    },
-    {
-      type: "number",
-      symbol: "2",
-      name: "two",
-    },
-    {
-      type: "number",
-      symbol: "3",
-      name: "three",
-    },
-    {
-      type: "operator",
-      symbol: "+",
-      name: "add",
-    },
-    {
-      type: "empty",
-      symbol: "",
-      name: "empty",
-    },
-    {
-      type: "number",
-      symbol: "0",
-      name: "zero",
-    },
-    {
-      type: "empty",
-      symbol: "",
-      name: "empty",
-    },
-    {
-      type: "operator",
-      symbol: "=",
-      name: "equals",
-    },
-  ];
 
   return (
     <div className="keyboard">
@@ -123,5 +20,108 @@ function Keyboard({ onButtonPressed }) {
     </div>
   );
 }
+
+const KEYS = [
+  {
+    type: "operator",
+    symbol: "CE",
+    name: "clear-entry",
+  },
+  {
+    type: "operator",
+    symbol: "C",
+    name: "clear-all",
+  },
+  {
+    type: "operator",
+    symbol: "%",
+    name: "percent",
+  },
+  {
+    type: "operator",
+    symbol: "÷",
+    name: "divide",
+  },
+  {
+    type: "number",
+    symbol: "7",
+    name: "seven",
+  },
+  {
+    type: "number",
+    symbol: "8",
+    name: "eight",
+  },
+  {
+    type: "number",
+    symbol: "9",
+    name: "nine",
+  },
+  {
+    type: "operator",
+    symbol: "×",
+    name: "multiply",
+  },
+  {
+    type: "number",
+    symbol: "4",
+    name: "four",
+  },
+  {
+    type: "number",
+    symbol: "5",
+    name: "five",
+  },
+  {
+    type: "number",
+    symbol: "6",
+    name: "six",
+  },
+  {
+    type: "operator",
+    symbol: "-",
+    name: "subtract",
+  },
+  {
+    type: "number",
+    symbol: "1",
+    name: "one",
+  },
+  {
+    type: "number",
+    symbol: "2",
+    name: "two",
+  },
+  {
+    type: "number",
+    symbol: "3",
+    name: "three",
+  },
+  {
+    type: "operator",
+    symbol: "+",
+    name: "add",
+  },
+  {
+    type: "empty",
+    symbol: "",
+    name: "empty",
+  },
+  {
+    type: "number",
+    symbol: "0",
+    name: "zero",
+  },
+  {
+    type: "empty",
+    symbol: "",
+    name: "empty",
+  },
+  {
+    type: "operator",
+    symbol: "=",
+    name: "equals",
+  },
+];
 
 export default Keyboard;
