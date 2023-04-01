72ed84e76057a26d79a69412df504a24c9cb295d
eniolajayi <ajayieniola16@gmail.com>
2023-04-01 13:40:34 +0100

    create result section

--- main.jsx
+++ main.jsx
@@ -1,7 +1,18 @@
 import { createRoot } from "react-dom/client";
 
 function Calculator() {
-  return <h1>Calculator</h1>;
+  return (
+    <div>
+      <div className="result">
+        <div className="history">
+          <div className="history__value"></div>
+        </div>
+        <div className="output">
+          <p className="output__value"></p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 const root = createRoot(document.getElementById("calculator"));
