cf31a61cd3d20f75201120991f12c8d8c6aa870d
eniolajayi <ajayieniola16@gmail.com>
2023-04-01 02:45:09 +0100

    target proper id

--- main.jsx
+++ main.jsx
@@ -1,4 +1,4 @@
 import { createRoot } from 'react-dom/client';
 
-const root = createRoot(document.getElementById('app'));
+const root = createRoot(document.getElementById('calculator'));
 root.render(<h1>Calculator</h1>);
