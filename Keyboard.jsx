eb0de3a2e54b5ad6ca450eee4d8a2cb47a5636b3
eniolajayi <ajayieniola16@gmail.com>
2023-08-01 04:57:45 +0100

    adjust keyboard template

--- Keyboard.jsx
+++ Keyboard.jsx
@@ -1,127 +1,26 @@
-function Keyboard({ onButtonPressed, buttonKeys = KEYS }) {
+import keys from "./data";
+
+function Keyboard({ onButtonPressed }) {
   const handleButtonClick = (event, data) => {
     onButtonPressed(data);
   };
 
   return (
     <div className="keyboard">
-      {buttonKeys.map((k, idx) => {
+      {keys.map((k, idx) => {
         return (
           <button
             key={idx}
-            data-keytype={k.type}
+            data-action={k.type === "operator" ? k.name : k.type}
             className={`key ${k.type}`}
             onClick={(e) => handleButtonClick(e, k)}
           >
-            {k.symbol}
+            {k.symbol === "*" ? <>&times;</> : k.symbol}
           </button>
         );
       })}
     </div>
   );
 }
-
-const KEYS = [
-  {
-    type: "operator",
-    symbol: "CE",
-    name: "clear-entry",
-  },
-  {
-    type: "operator",
-    symbol: "C",
-    name: "clear-all",
-  },
-  {
-    type: "operator",
-    symbol: "%",
-    name: "percent",
-  },
-  {
-    type: "operator",
-    symbol: "÷",
-    name: "divide",
-  },
-  {
-    type: "number",
-    symbol: "7",
-    name: "seven",
-  },
-  {
-    type: "number",
-    symbol: "8",
-    name: "eight",
-  },
-  {
-    type: "number",
-    symbol: "9",
-    name: "nine",
-  },
-  {
-    type: "operator",
-    symbol: "*",
-    name: "multiply",
-  },
-  {
-    type: "number",
-    symbol: "4",
-    name: "four",
-  },
-  {
-    type: "number",
-    symbol: "5",
-    name: "five",
-  },
-  {
-    type: "number",
-    symbol: "6",
-    name: "six",
-  },
-  {
-    type: "operator",
-    symbol: "-",
-    name: "subtract",
-  },
-  {
-    type: "number",
-    symbol: "1",
-    name: "one",
-  },
-  {
-    type: "number",
-    symbol: "2",
-    name: "two",
-  },
-  {
-    type: "number",
-    symbol: "3",
-    name: "three",
-  },
-  {
-    type: "operator",
-    symbol: "+",
-    name: "add",
-  },
-  {
-    type: "empty",
-    symbol: "",
-    name: "empty",
-  },
-  {
-    type: "number",
-    symbol: "0",
-    name: "zero",
-  },
-  {
-    type: "empty",
-    symbol: "",
-    name: "empty",
-  },
-  {
-    type: "operator",
-    symbol: "=",
-    name: "equals",
-  },
-];
 
 export default Keyboard;
