eb640bf8e637e7195f254db65c387d32099ae6a4
eniolajayi <ajayieniola16@gmail.com>
2023-08-01 04:58:07 +0100

    put keyboard key data is seperate file

---
+++ data.js
@@ -0,0 +1,97 @@
+const keys = [
+    {
+        type: "operator",
+        symbol: "+",
+        name: "add",
+    },
+    {
+        type: "operator",
+        symbol: "-",
+        name: "subtract",
+    },
+    {
+        type: "operator",
+        symbol: "*",
+        name: "multiply",
+    },
+    {
+        type: "operator",
+        symbol: "÷",
+        name: "divide",
+    },
+
+    {
+        type: "number",
+        symbol: "7",
+        name: "seven",
+    },
+    {
+        type: "number",
+        symbol: "8",
+        name: "eight",
+    },
+    {
+        type: "number",
+        symbol: "9",
+        name: "nine",
+    },
+
+    {
+        type: "number",
+        symbol: "4",
+        name: "four",
+    },
+    {
+        type: "number",
+        symbol: "5",
+        name: "five",
+    },
+    {
+        type: "number",
+        symbol: "6",
+        name: "six",
+    },
+    {
+        type: "number",
+        symbol: "1",
+        name: "one",
+    },
+    {
+        type: "number",
+        symbol: "2",
+        name: "two",
+    },
+    {
+        type: "number",
+        symbol: "3",
+        name: "three",
+    },
+
+    {
+        type: "number",
+        symbol: "0",
+        name: "zero",
+    },
+    {
+        type: "operator",
+        symbol: ".",
+        name: "decimal",
+    },
+    {
+        type: "operator",
+        symbol: "C",
+        name: "clear-all",
+    },
+    {
+        type: "operator",
+        symbol: "CE",
+        name: "clear-entry",
+    },
+    {
+        type: "operator",
+        symbol: "=",
+        name: "equals",
+    },
+];
+
+export default keys;
