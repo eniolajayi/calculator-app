753fa99c4948702e39fb4f11a4746535c8431f94
eniolajayi <ajayieniola16@gmail.com>
2023-08-07 18:48:49 +0100

    fix: add strict mode

--- main.jsx
+++ main.jsx
@@ -1,6 +1,11 @@
 import "./style.css";
 import { createRoot } from "react-dom/client";
 import Calculator from "./Calculator";
+import { StrictMode } from "react";
 
 const root = createRoot(document.getElementById("calculator"));
-root.render(<Calculator />);
+root.render(
+  <StrictMode>
+    <Calculator />
+  </StrictMode>
+);
