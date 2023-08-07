8b4fd125b8f4a9ae6d840f4bbd351871aa419ce4
eniolajayi <ajayieniola16@gmail.com>
2023-08-07 18:06:41 +0100

    fix: broken import link

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ecsstatic from "@acab/ecsstatic";
+import { ecsstatic } from '@acab/ecsstatic/vite';
 
 export default defineConfig({
     plugins: [react(), ecsstatic()],
